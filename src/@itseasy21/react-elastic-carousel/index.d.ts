6fac8db1f233ba53621886000cc0383dd6dac5be
_xB <65196493+xBiei@users.noreply.github.com>
2022-10-16 07:09:46 +0300

    make isRTL optional

--- src/@itseasy21/react-elastic-carousel/index.d.ts
+++ src/@itseasy21/react-elastic-carousel/index.d.ts
@@ -35,7 +35,7 @@ export interface ReactElasticCarouselProps {
   // Defaults to false
   verticalMode?: boolean;
   // Defaults to false
-  isRTL: boolean;
+  isRTL?: boolean;
   // Defaults to true
   pagination?: boolean;
   // Defaults to 500
